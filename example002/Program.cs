165a79c9709f7a215925166bc2f52a669657295b
*Egor <egorkiper21@gmail.com>
2023-03-27 17:51:40 +0300

    дз

---
+++ example002/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int b = 0; b < numbers.Length; b+=2)
+    sum = sum + numbers[b];
+
+    Console.WriteLine($"Всего {numbers.Length} числа/ел, сумма элементов, cтоящих на нечётных индексах = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
